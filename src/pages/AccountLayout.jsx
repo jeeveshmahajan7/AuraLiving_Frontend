11291368ef6c8172a06c841e52f1ad7f0bd6e115
Jeevesh Mahajan <jeeveshmahajan00@gmail.com>
2025-09-18 00:41:56 +0530

    feat: add AccountLayout page with side navigation & nested routes

---
+++ src/pages/AccountLayout.jsx
@@ -0,0 +1,50 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+const AccountLayout = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <main className="container">
+        <h1 className="display-6 py-5">User Details</h1>
+
+        <div className="row">
+          <div className="col-md-3">
+            <ul className="list-group">
+              <Link
+                to="profile"
+                className={`list-group-item ${
+                  location.pathname.includes("profile") ? "active-link" : ""
+                }`}
+              >
+                Account
+              </Link>
+              <Link
+                to="orders"
+                className={`list-group-item ${
+                  location.pathname.includes("orders") ? "active-link" : ""
+                }`}
+              >
+                Order History
+              </Link>
+              <Link
+                to="address/new"
+                className={`list-group-item ${
+                  location.pathname.includes("address/new") ? "active-link" : ""
+                }`}
+              >
+                Add New Address
+              </Link>
+            </ul>
+          </div>
+
+          <div className="col-md-9">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AccountLayout;
