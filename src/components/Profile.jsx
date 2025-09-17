2454a12b8e7b69e908e9edfb30b0f85a5175d392
Jeevesh Mahajan <jeeveshmahajan00@gmail.com>
2025-09-18 00:43:04 +0530

    feat: add Profile page with static user details

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+const Profile = () => {
+  return (
+    <div>
+      <h5 className="mb-4">Profile Details</h5>
+      <p>Full Name: Jeevesh Mahajan</p>
+      <p>Email: jeeveshmahajan00@gmail.com</p>
+      <p>Phone Number: 7788899777</p>
+      <p>Address: 123 Street, Navi Mumbai, Maharashtra - 400701</p>
+      {/* <button className="btn btn-custom">Edit</button> */}
+    </div>
+  );
+};
+
+export default Profile;
